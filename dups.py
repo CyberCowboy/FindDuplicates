86e93ec06af47838a66ec34b14b44db913bdb86d
Bryan James <bjames@jamesgang.dyndns.org>
2013-06-10 13:45:30 -0500

    added comments (NOT WORKING VERSION)
    I added comments primarily so that I could see what is happening,
    however I've noticed the returned txt file contains ALL the files that
    were compared, not just the duplicates.  That hasn't yet been fixed, so
    this version (as well as the last one) should be considered
    non-functional.

--- dups.py
+++ dups.py
@@ -4,36 +4,35 @@ hashlist = {}  # content signature -> list of filenames
 dups = []
 
 def get_hash(rootdir):
-#	"""goes through directory tree, compares md5 hash of all files,
-#	combines files with same hash value into list in hashmap directory"""
-	#try:
-	for path, dirs, files in os.walk(rootdir):
-	    for filename in files:
-	        fullname = os.path.join(path, filename)
-	        with open(fullname) as f:
-	            md5 = hashlib.md5()
-	            while True:
-	            	d = f.read(4096)
-            		if not d:
-            			break
-        			md5.update(d)
-	            h = md5.hexdigest()         
-	            filelist = hashlist.setdefault(h, [])         
-	            filelist.append(fullname)   
+#   """goes through directory tree, compares md5 hash of all files,
+#   combines files with same hash value into list in hashmap directory"""
+    for path, dirs, files in os.walk(rootdir):
+        #this section goes through the given directory, and all subdirectories/files below
+        #as part of a loop reading them in
+        for filename in files:
+            #steps through each file and starts the process of getting the MD5 hashes for the file
+            #comparing that hash to known hashes that have already been calculated and either merges it
+            #with the known hash (which indicates duplicates) or adds it so that it can be compared to future
+            #files
+            fullname = os.path.join(path, filename)
+            with open(fullname) as f:
+                #does the actual hashing
+                md5 = hashlib.md5()
+                while True:
+                    d = f.read(4096)
+                    if not d:
+                        break
+                    md5.update(d)
+                h = md5.hexdigest()         
+                filelist = hashlist.setdefault(h, [])         
+                filelist.append(fullname)   
 
-	for x in hashlist:
-		currenthash = hashlist[x]
-		if len(currenthash) > 1:
-			dups.append(currenthash)
-			#print currenthash
-		#else:
-		#	print "nothing to do here"
-
-	output = open('duplicates.txt','w')
-	output.write(str(dups))
-	output.close()
-			
-			
-	#except:
-	#	print "something went pear-shaped: "
-	#	pass
+    for x in hashlist:
+        currenthash = hashlist[x]
+        #goes through and if has has more than one file listed with it
+        #considers it a duplicate and adds it to the output list
+        if len(currenthash) > 1:
+            dups.append(currenthash)
+    output = open('duplicates.txt','w')
+    output.write(str(dups))
+    output.close()
